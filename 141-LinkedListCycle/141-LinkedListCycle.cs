dd94cb238e453936337ec3540e7bb56821d9f790
inderbindra <inderbindra@gmail.com>
2025-12-11 20:02:39 -0500

    Create 141-LinkedListCycle

---
+++ 141-LinkedListCycle/141-LinkedListCycle.cs
@@ -0,0 +1,29 @@
+// Last updated: 12/11/2025, 8:02:41 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
