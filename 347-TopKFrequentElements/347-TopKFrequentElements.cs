38229fb447542d140833254fe1783c2e66c32b95
inderbindra <inderbindra@gmail.com>
2025-12-12 20:23:17 -0500

    File updated at 12/12/2025, 8:23:17 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.cs
+++ 347-TopKFrequentElements/347-TopKFrequentElements.cs
@@ -1,27 +1,26 @@
-// Last updated: 12/12/2025, 8:17:25 PM
+// Last updated: 12/12/2025, 8:23:17 PM
 1public class Solution {
 2    public int[] TopKFrequent(int[] nums, int k) {
-3        List<int> result = new();
-4        PriorityQueue<int, int> pq = new();
-5        Dictionary<int, int> dict = new();
-6        int i=0;
-7        for( i=0; i<nums.Length; i++)
-8        {
-9            dict.TryAdd(nums[i], 0);
-10            dict[nums[i]] += 1;
-11        }
-12        foreach(var item in dict)
-13        {
-14            pq.Enqueue(item.Key, item.Value);
-15            if(pq.Count>0 && pq.Count > k)
-16            {
-17                pq.Dequeue();
-18            }
-19        }
-20        while(pq.Count > 0)
-21        {
-22            result.Add(pq.Dequeue());
-23        }
-24        return result.ToArray();
-25    }
-26}
+3        PriorityQueue<int, int> pq = new();
+4        Dictionary<int, int> hashMap = new();
+5        for(int i=0; i<nums.Length; i++)
+6        {
+7            hashMap.TryAdd(nums[i], 0);
+8            hashMap[nums[i]] += 1;
+9        }
+10        foreach(var item in hashMap)
+11        {
+12            pq.Enqueue(item.Key, item.Value);
+13            while(pq.Count > 0 && pq.Count > k)
+14            {
+15                pq.Dequeue();
+16            }
+17        }
+18        List<int> result = new();
+19        while(pq.Count > 0)
+20        {
+21            result.Add(pq.Dequeue());
+22        }
+23        return result.ToArray();
+24    }
+25}
