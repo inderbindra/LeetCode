1cdecb5d85bb039337f8e91e93a6b1a3c450ef4a
inderbindra <inderbindra@gmail.com>
2025-12-15 20:06:44 -0500

    File updated at 12/15/2025, 8:06:43 PM

--- 125-ValidPalindrome/125-ValidPalindrome.cs
+++ 125-ValidPalindrome/125-ValidPalindrome.cs
@@ -1,25 +1,26 @@
-// Last updated: 12/11/2025, 8:02:51 PM
-public class Solution {
-    public bool IsPalindrome(string s) {
-        StringBuilder str = new();
-
-        for(int i=0; i<s.Length; i++)
-        {
-            if(char.IsLetterOrDigit(s[i]))
-            {
-                str.Append(char.ToLower(s[i]));
-            }
-        }
-        int left=0, right = str.Length-1;
-        while(left <= right)
-        {
-            if(str[left] != str[right])
-            {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-    }
-}
+// Last updated: 12/15/2025, 8:06:43 PM
+1public class Solution {
+2    public bool IsPalindrome(string s) {
+3        
+4        StringBuilder str = new();
+5        for(int i=0; i<s.Length; i++)
+6        {
+7            if(char.IsLetterOrDigit(s[i]))
+8            {
+9                str.Append(char.ToLower(s[i]));
+10            }
+11        }
+12        int left=0, right = str.Length-1;
+13
+14        while(left < right)
+15        {
+16            if(str[left] != str[right])
+17            {
+18                return false;
+19            }
+20            left++;
+21            right--;
+22        }
+23        return true;
+24    }
+25}
