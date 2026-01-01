0ecc265f63f8aa2f9832a3021c8370258b5c6233
inderbindra <inderbindra@gmail.com>
2026-01-01 15:43:04 -0500

    File updated at 1/1/2026, 3:43:07 PM

--- 206-ReverseLinkedList/206-ReverseLinkedList.cs
+++ 206-ReverseLinkedList/206-ReverseLinkedList.cs
@@ -1,27 +1,27 @@
-// Last updated: 12/11/2025, 8:02:24 PM
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public int val;
- *     public ListNode next;
- *     public ListNode(int val=0, ListNode next=null) {
- *         this.val = val;
- *         this.next = next;
- *     }
- * }
- */
-public class Solution {
-    public ListNode ReverseList(ListNode head) {
-        ListNode curr = head;
-        ListNode node = null;
-        ListNode prev = null;
-        while(curr != null)
-        {
-            node = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = node;
-        }
-        return prev;
-    }
-}
+// Last updated: 1/1/2026, 3:43:07 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     public int val;
+5 *     public ListNode next;
+6 *     public ListNode(int val=0, ListNode next=null) {
+7 *         this.val = val;
+8 *         this.next = next;
+9 *     }
+10 * }
+11 */
+12public class Solution {
+13    public ListNode ReverseList(ListNode head) {
+14        ListNode currentNode = head;
+15        ListNode previousNode = null, node=null;
+16
+17        while(currentNode != null)
+18        {
+19            node = currentNode.next;
+20            currentNode.next = previousNode;
+21            previousNode = currentNode;
+22            currentNode = node;
+23        }
+24        return previousNode;
+25    }
+26}
