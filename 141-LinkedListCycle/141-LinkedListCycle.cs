f2426d0b7a7a72349d56cf0f68beba4defb1afba
inderbindra <inderbindra@gmail.com>
2026-01-02 20:02:44 -0500

    File updated at 1/2/2026, 8:02:44 PM

--- 141-LinkedListCycle/141-LinkedListCycle.cs
+++ 141-LinkedListCycle/141-LinkedListCycle.cs
@@ -1,29 +1,29 @@
-// Last updated: 12/11/2025, 8:02:41 PM
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public int val;
- *     public ListNode next;
- *     public ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-public class Solution {
-    public bool HasCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while(fast != null && fast.next != null)
-        {
-            slow = slow.next;
-            fast = fast.next.next;
-            if(slow == fast)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-}
+// Last updated: 1/2/2026, 8:02:44 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     public int val;
+5 *     public ListNode next;
+6 *     public ListNode(int x) {
+7 *         val = x;
+8 *         next = null;
+9 *     }
+10 * }
+11 */
+12public class Solution {
+13    public bool HasCycle(ListNode head) {
+14        ListNode slow = head;    
+15        ListNode fast = head;
+16
+17        while(fast != null && fast.next != null)
+18        {
+19            slow = slow.next;
+20            fast = fast.next.next;
+21            if(slow == fast)
+22            {
+23                return true;
+24            }
+25        }
+26        return false;
+27    }
+28}
