f8fc439b9b85e88c57f9046d0d77a0ec859fa87d
inderbindra <inderbindra@gmail.com>
2026-01-06 20:25:48 -0500

    File updated at 1/6/2026, 8:25:51 PM

--- 133-CloneGraph/133-CloneGraph.cs
+++ 133-CloneGraph/133-CloneGraph.cs
@@ -1,55 +1,54 @@
-// Last updated: 12/11/2025, 8:02:46 PM
-/*
-// Definition for a Node.
-public class Node {
-    public int val;
-    public IList<Node> neighbors;
-
-    public Node() {
-        val = 0;
-        neighbors = new List<Node>();
-    }
-
-    public Node(int _val) {
-        val = _val;
-        neighbors = new List<Node>();
-    }
-
-    public Node(int _val, List<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-*/
-
-public class Solution {
-    Dictionary<int, Node> hashMap;
-    public Node CloneGraph(Node node) {
-        hashMap = new();
-        return Clone(node);
-    }
-    private Node Clone(Node node)
-    {
-        Node newNode = null;
-        if(node != null)
-        {
-            if(!hashMap.ContainsKey(node.val))
-            {
-                newNode = new Node(node.val);
-                hashMap.Add(node.val, newNode);
-                if(node.neighbors.Count > 0)
-                {
-                    for(int i=0; i<node.neighbors.Count; i++)
-                    {
-                        newNode.neighbors.Add(Clone(node.neighbors[i]));
-                    }
-                }
-            }
-            else
-            {
-                newNode = hashMap[node.val];
-            }
-        }
-        return newNode;
-    }
-}
+// Last updated: 1/6/2026, 8:25:51 PM
+1/*
+2// Definition for a Node.
+3public class Node {
+4    public int val;
+5    public IList<Node> neighbors;
+6
+7    public Node() {
+8        val = 0;
+9        neighbors = new List<Node>();
+10    }
+11
+12    public Node(int _val) {
+13        val = _val;
+14        neighbors = new List<Node>();
+15    }
+16
+17    public Node(int _val, List<Node> _neighbors) {
+18        val = _val;
+19        neighbors = _neighbors;
+20    }
+21}
+22*/
+23
+24public class Solution {
+25    Dictionary<Node, Node> hashMap = new();
+26    public Node CloneGraph(Node node) {
+27        if(node == null)
+28        {
+29            return null;
+30        }
+31        DFS(node);
+32        return hashMap[node];
+33    }
+34
+35    private void DFS(Node node)
+36    {
+37        if(node == null || hashMap.ContainsKey(node))
+38        {
+39            return;
+40        }
+41
+42        Node newNode = new Node(node.val);
+43        hashMap.TryAdd(node, newNode);
+44        if(node.neighbors != null)
+45        {
+46            for(int i=0; i<node.neighbors.Count; i++)
+47            {
+48                DFS(node.neighbors[i]);
+49                newNode.neighbors.Add(hashMap[node.neighbors[i]]);
+50            }
+51        }
+52    }
+53}
