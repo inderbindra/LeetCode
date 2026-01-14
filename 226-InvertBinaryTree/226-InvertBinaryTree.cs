381eecf41bc6554828e301bacf4756f98b3fb163
inderbindra <inderbindra@gmail.com>
2026-01-14 16:33:52 -0500

    File updated at 1/14/2026, 4:33:59 PM

--- 226-InvertBinaryTree/226-InvertBinaryTree.cs
+++ 226-InvertBinaryTree/226-InvertBinaryTree.cs
@@ -1,27 +1,29 @@
-// Last updated: 12/11/2025, 8:02:09 PM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-public class Solution {
-    public TreeNode InvertTree(TreeNode root) {
-        if(root == null)
-        {
-            return null;
-        }
-        TreeNode left = InvertTree(root.right);
-        TreeNode right = InvertTree(root.left);
-        root.left = left;
-        root.right = right;
-        return root;
-    }
-}
+// Last updated: 1/14/2026, 4:33:59 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     public int val;
+5 *     public TreeNode left;
+6 *     public TreeNode right;
+7 *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+8 *         this.val = val;
+9 *         this.left = left;
+10 *         this.right = right;
+11 *     }
+12 * }
+13 */
+14public class Solution {
+15    public TreeNode InvertTree(TreeNode root) {
+16        if(root == null)    
+17        {
+18            return null;
+19        }
+20        TreeNode left = InvertTree(root.left);
+21        TreeNode right = InvertTree(root.right);
+22        root.left = right;
+23        root.right = left;
+24        return root;
+25    }
+26
+27
+28}
